77f19bd6fbfaeedd8a544fb3ff5b929cfafddd68
amanaligit <ali.aman.2010@gmail.com>
2021-01-24 22:48:34 +0530

    added docker-compose running mongodb and web-app and dockerfile for image of the node server.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'mongoUrl': "mongodb://mongo/conFusion",
     'facebook': {
         clientId: '437689060637906',
         clientSecret: 'b4f43e5e4872b329fc80a383bbf4cc8e'
